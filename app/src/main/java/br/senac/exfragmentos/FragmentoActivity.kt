847b7e6b0737012c6859182048c5a8b93e92c9a4
GuilhermePCastro <guibkk@gmail.com>
2021-09-24 19:39:17 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/exfragmentos/FragmentoActivity.kt
@@ -0,0 +1,23 @@
+package br.senac.exfragmentos
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.exfragmentos.databinding.ActivityFragmentoBinding
+
+class FragmentoActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityFragmentoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFragmentoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAlbum.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.ctnAlbum, AlbumFragment()).commit()
+
+        }
+    }
+}
